4ffcdf4998d68624d8d9ca647502873d0e73d51b
miguelsalamanca007 <miguelangel62009@hotmail.com>
2023-09-21 21:13:58 -0500

    first commit

---
+++ src/main/java/com/example/ietipostgres/IetiPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.example.ietipostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IetiPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IetiPostgresApplication.class, args);
+	}
+
+}
